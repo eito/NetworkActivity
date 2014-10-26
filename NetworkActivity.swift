c58c8fd253b941682ddba9796f9cd4078a2edb27
Eric Ito <eric.ito@gmail.com>
2014-10-26 13:51:15 -0700

    add license and initial commit

---
+++ NetworkActivity.swift
@@ -0,0 +1,32 @@
+//
+//  NetworkActivity.swift
+//
+//  Created by Eric Ito on 10/21/14.
+//  Copyright (c) 2014 Eric Ito. All rights reserved.
+//
+
+import Foundation
+
+var NetworkActivityCount = 0
+let serialQueue = dispatch_queue_create("com.networkactivity.queue", DISPATCH_QUEUE_SERIAL);
+
+public class NetworkActivity {
+    
+    public class func start() {
+        dispatch_async(serialQueue) {
+            NetworkActivityCount++
+            NetworkActivity.updateNetworkStatus()
+        }
+    }
+    
+    public class func stop() {
+        dispatch_async(serialQueue) {
+            NetworkActivityCount--
+            NetworkActivity.updateNetworkStatus()
+        }
+    }
+    
+    private class func updateNetworkStatus() {
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = (NetworkActivityCount > 0)
+    }
+}
